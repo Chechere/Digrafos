f2d1a0d9b0b54a767de02c55c32787f9725d534c
Cesar <cesartjmv@gmail.com>
2022-12-02 10:19:33 +0100

    Intercambio inicial de proyecto

---
+++ Arista.java
@@ -0,0 +1,11 @@
+
+/**
+ * Write a description of class Arista here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Arista {
+    private double peso;
+    private int nodoI, nodoF;
+}
